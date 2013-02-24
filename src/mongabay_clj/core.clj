e90e82727ad976e350ac0e132e936aaf51fdeb71
Dan Hammer <dan.s.hammer@gmail.com>
2013-02-24 15:01:50 -0800

    major clean-up, everything works

--- src/mongabay_clj/core.clj
+++ src/mongabay_clj/core.clj
@@ -36,45 +36,43 @@
   [m ks f & args]
   (reduce #(apply update-in %1 [%2] f args) m ks))
 
+(defn prep-entry
+  "accepts a map and returns a restricted and cleaned version for
+  translation into the SQL query"
+  [m]
+  (-> (select-keys m field-vec)
+      (doto-map [:title :description] clean-str)))
+
 (defn convert-entries
   "accepts a collection of maps and converts them into the appropriate
   clojure data structures."
   [coll]
-  (let [updated-map (map #(doto-map % [:keywords] prep-vals) coll)]
-    (concat [(keys (first coll))]
-            (map vals updated-map))))
-
-(defn clean-collection
-  "Remove single apostrophes from the title and description values in
-  the supplied collection of hash maps"
-  [coll]
-  (map #(doto-map % [:title :description] clean-str) coll))
-
-(defn add-field
-  "accepts a hash map and a new description value, wrapped in a
-  vector, and replaces the old description value"
-  [m [new-desc]]
-  (assoc m :description new-desc))
-
-(defn convert-this
-  "accepts a collection of maps and converts them into the appropriate
-  clojure data structures."
-  [coll]
-  (let [desc (map (comp vector str :description) (clean-collection coll))
-        up-coll (map add-field coll desc)
-        new-coll (map #(select-keys % field-vec) up-coll)
-        title (vec (keys (first new-coll)))]
-    (concat [title]
-            (map (comp vec vals) new-coll))))
+  (let [sub-coll (map prep-entry coll)]
+    (concat [(-> sub-coll first keys vec)]
+            (map (comp vec vals) sub-coll))))
 
 (defn upload-stories
   "grabs the JSON of the stories with the supplied keys, translates
   the resulting string from the post request, and then uploads the
   specified fields to our cartodb table; will overwrite existing table
-  and then georeference the latitude and longitude fields"
-  []
-  (let [data (convert-this (mongabay-query))]
-    (do (apply insert-rows "mongabay" creds "mongabaydb" data)
-        (carto/query "UPDATE mongabaydb SET the_geom=cdb_latlng(lat,lon)" "mongabay"
-                     :oauth creds))))
+  and then georeference the latitude and longitude fields.
+
+  Example usage:
+    (upload-stories \"monga_test\")
+    => {:time 0.061, :total_rows 331, :rows []}"
+  [table-name]
+  (let [data (convert-entries (mongabay-query))]
+    (do
+
+      ;; delete existing entries
+      (delete-all "mongabay" creds table-name)
+
+      ;; insert the new stories as rows into the cartodb table
+      (apply insert-rows "mongabay" creds table-name data)
+
+      ;; georeference the table using the coordinate variables named
+      ;; lat and lon
+      (carto/query
+       (str "UPDATE " table-name " SET the_geom=cdb_latlng(lat,lon)")
+       "mongabay" :oauth creds))))
 
