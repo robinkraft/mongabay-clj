9008629f70d2ea2997cd31b8147e51c04e3352d2
Robin Kraft <rkraft4@gmail.com>
2013-07-09 10:37:02 -0700

    Add author and updated fields to table; modified CartoDB table accordingly

--- src/mongabay_clj/core.clj
+++ src/mongabay_clj/core.clj
@@ -18,7 +18,7 @@
 
 (def field-vec
   "vector of fields (as keywords) to be uploaded to cartodb"
-  [:guid :loc :lat :lon :title :thumbnail :description :published]) ;; :keywords :author :updated
+  [:guid :loc :lat :lon :title :thumbnail :description :published :author :updated]) ;; :keywords 
 
 (def mongabay-url
   "a JSON endpoint for all mongabay articles with geo-coordinates"
