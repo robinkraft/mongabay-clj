5e6f525906c8c3eb76faaf3081977ce276d541ad
Robin Kraft <rkraft4@gmail.com>
2013-11-25 23:30:48 +0100

    Convert published field from date w/month names to proper dates

--- test/mongabay_clj/core_test.clj
+++ test/mongabay_clj/core_test.clj
@@ -2,6 +2,10 @@
   (:use mongabay-clj.core
         [midje sweet]))
 
-(fact
+(future-fact
   (let [return-map (upload-stories "monga_test")]
     (>  (:total_rows return-map) 0)) => true)
+
+(fact "Check `date-ify`"
+  (date-ify "December 25, 1999") => "1999-12-25"
+  (date-ify "12 25, 1999") => (throws IllegalArgumentException))
